df3bbdf219ecad7043832ef71e0031dcb8b59880
herbal-egress <herbal_egress@gmail.com>
2026-03-29 20:27:00 +0300

    Демонстрационный UI. React запустился

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,23 @@
-// frontend/src/App.tsx
-import { Routes, Route } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
+import CounterpartyForm from './components/CounterpartyForm';
+import InvoiceForm from './components/InvoiceForm';
 import PayerForm from './components/PayerForm';
 import Vedomost from './components/Vedomost';
-import InvoiceForm from './components/InvoiceForm';
-import CounterpartyForm from './components/CounterpartyForm';
 
 import DeviationCard from './components/stubs/DeviationCard';
 import DeviationRegistry from './components/stubs/DeviationRegistry';
-import OrderToCounterparty from './components/stubs/OrderToCounterparty';
-import OrdersRegistry from './components/stubs/OrdersRegistry';
 import InvoicesRegistry from './components/stubs/InvoicesRegistry';
 import Memo from './components/stubs/Memo';
 import MemosRegistry from './components/stubs/MemosRegistry';
+import OrderToCounterparty from './components/stubs/OrderToCounterparty';
+import OrdersRegistry from './components/stubs/OrdersRegistry';
 
 function App() {
     return (
         <div className="flex h-screen bg-gray-50">
             <Sidebar />
+
             <main className="flex-1 overflow-auto p-8">
                 <Routes>
                     <Route path="/" element={<Vedomost />} />
@@ -25,7 +25,6 @@ function App() {
                     <Route path="/invoice" element={<InvoiceForm />} />
                     <Route path="/counterparty" element={<CounterpartyForm />} />
 
-                    {/* ЗАГОТОВКИ */}
                     <Route path="/deviation-card" element={<DeviationCard />} />
                     <Route path="/deviation-registry" element={<DeviationRegistry />} />
                     <Route path="/order-counterparty" element={<OrderToCounterparty />} />
@@ -33,6 +32,8 @@ function App() {
                     <Route path="/invoices-registry" element={<InvoicesRegistry />} />
                     <Route path="/memo" element={<Memo />} />
                     <Route path="/memos-registry" element={<MemosRegistry />} />
+
+                    <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
             </main>
         </div>
